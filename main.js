dd191db6c603e89c45bb0626d058d2a1eb159d29
Benny Ho <benny.ho2530@gmail.com>
2020-04-01 17:40:10 +0200

    added solution ex 3

--- main.js
+++ main.js
@@ -46,14 +46,81 @@ console.log(isEqualTo100(28,68))
 Write a Javacsript program to get the extenstion of a filename.
 */
 
-
+/*
 const getFileExtension = (str) => 
 
 str.slice(str.lastIndexOf('.'));
 
 console.log(getFileExtension('index.html'));
 console.log(getFileExtension('webpack.config.js'));
+*/
+/* Exercise 3:
+--------------
+
+Write a Javascript program to replace every character in a given string
+with the character following it in the alphabet
+*/
+
+// String.fromCharCode
+// charCodeAt
+
+const moveCharsForward = (str) => 
+    str.split('').map(char => String.fromCharCode(char.charCodeAt(0) + 1)).join('');
 
 
+console.log(moveCharsForward('abcd'));
+    /*
+function replaceChar(str) {
+
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+    // string to array
+
+    const array = str.split('');
+    console.log(array);
+
+    
+    // select each letter from string
+    
+    array.forEach(letter => {
+        // console.log(letter);
+
+        // replace each letter with a character following in the alphabet
+
+        let position = alphabet.indexOf(letter);
+        // console.log('current position ' + position);
+
+        let newPosition = position + 1;
+        // console.log( 'new position ' + newPosition);
+        
+        let newLetter = alphabet[newPosition];
+       
+        console.log(typeof newLetter);
+
+        
+        const newWord = newLetter.toString();
+        console.log(newWord);
+        
+    })
+    
+
+    for (var i = 0; i < array.length; i++) {
+
+        // console.log(array[i])
+        let position = alphabet.indexOf(array[i]);
+         // console.log('current position ' + position);
+
+         let newPosition = position + 1;
+         // console.log( 'new position ' + newPosition);
+         
+         let newLetter = alphabet[newPosition];
+         console.log(newLetter)
+       
+ 
+    }
 
 
+}
+
+console.log(replaceChar('hello'));
+*/
